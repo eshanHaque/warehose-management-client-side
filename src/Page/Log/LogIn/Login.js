58a4a8e76c6295f4b958cdca93e2d4aaf12ff30c
eshanHaque <eshan.haque16@gmail.com>
2022-05-07 17:28:19 +0600

    login emplement done

--- src/Page/Log/LogIn/Login.js
+++ src/Page/Log/LogIn/Login.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email =emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+    }
+    const navigateRegister = event => {
+        navigate('/register')
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -15,7 +30,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
@@ -24,6 +39,7 @@ const Login = () => {
                     Submit
                 </Button>
             </Form>
+            <p>Not a Member? <span onClick={navigateRegister} className='text-danger'>Register now</span> for <b>Membership</b> </p>
         </div>
     );
 };
