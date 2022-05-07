6a10d3132af7ac69592d5cc8e12c19c511a9295c
eshanHaque <eshan.haque16@gmail.com>
2022-05-07 21:29:55 +0600

    social login done with google

--- src/Page/Log/SocialLogin/SocialLogin.js
+++ src/Page/Log/SocialLogin/SocialLogin.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+
+    if (error) {
+        errorElement = <div>
+            <p className='text-danger'>Error: {error.message}</p>
+        </div>
+    }
+    if (user) {
+        navigate('/home');
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
-                <div style={{height:'1px'}} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
                 <p className='mt-2 px-2'>or</p>
-                <div style={{height:'1px'}} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
+            {errorElement}
             <div>
-                <button className='btn btn-primary d-block mx-auto w-50 '>Google Sign In</button>
+                <button
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-primary d-block mx-auto w-50 '>Google Sign In</button>
             </div>
         </div>
     );
